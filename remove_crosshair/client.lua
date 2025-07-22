7f901ee69182cff2a40e98fd4a41ba5a85eef12d
amadovon <august.rasmussen1@gmail.com>
2025-07-22 13:22:49 +0200

    Add files via upload

---
+++ remove_crosshair/client.lua
@@ -0,0 +1,7 @@
+Citizen.CreateThread(function()
+    while true do
+        Wait(0)
+        -- Skjul crosshair for ALLE våben
+        HideHudComponentThisFrame(14)
+    end
+end)
---
+++ remove_crosshair/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Chrisser'
+description 'Fjerner vanilla GTA5 crosshair'
+version '1.0.0'
+
+client_script 'client.lua'
